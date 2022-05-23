1070ec03a36256748ccd93ae4214fb7b67eb2ac0
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-23 18:18:03 -0500

    initial solution for deaf-grandma

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,20 @@
 function deafGrandma() {
-
+  let count = 0;
+  while (count < 2) {
+    let userInput = prompt("Enter message");
+    let capital = userInput.toUpperCase();
+    if (userInput === "") {
+      console.log(`WHAT?!`);
+    } else if (capital !== userInput) {
+      console.log(`SPEAK UP, KID!`);
+    } else if (userInput === "GOODBYE!") {
+      count++;
+      console.log(`LEAVING SO SOON?`);
+    } else if (capital === userInput) {
+      console.log(`NO, NOT SINCE 1946!`);
+    }
+  }
+  console.log(`LATER, SKATER!`);
 }
 
 deafGrandma();
