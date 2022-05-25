e691fe7e054e1c0a896dbaec2e51003daa9bce4e
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-25 10:43:39 -0500

    python solution for deaf-grandma

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,20 @@
 def deaf_grandma():
-
+    count = 0
+    print('HEY KID!')
+    while count < 2:
+        userInput = input("Enter message\n")
+        capital = userInput.upper()
+        if userInput == "":
+            print('WHAT?!')
+        elif capital != userInput:
+            print('SPEAK UP, KID')
+        elif userInput == 'GOODBYE!':
+            count += 1
+            if count < 2:
+                print('LEAVING SO SOON')
+        elif capital == userInput:
+            print('NO, NOT SINCE 1946!')
+    print('LATER, SKATER!')
 
 
 deaf_grandma()
